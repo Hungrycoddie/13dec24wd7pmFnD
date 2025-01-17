6dbf47f4bae1d0e6501816bde28cd41c61c5341e
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-01-17 20:44:08 +0530

    update

---
+++ Js/Day1/script.js
@@ -0,0 +1,153 @@
+// String;
+
+// const ar = 'Hello World';
+
+
+// Number;
+
+// let ra = 30000000000000n; 
+// console.log( typeof ra);
+
+
+
+// Bigint;
+// let lnum = 90071992547409922n;
+
+// console.log(lnum + 1n);
+
+
+
+// Boolean;
+
+
+// let ar = "true";
+// let ra = false;
+
+// console.log(typeof ra);
+
+
+// let isweekend  = false
+// let isHolliday = true
+
+// let canRest = isweekend || isHolliday;
+
+
+// console.log(canRest);
+
+
+
+
+
+// let isRaining = true;
+
+// let isNoraining = !isRaining; 
+
+// console.log(isNoraining);
+
+
+
+
+
+// Undefined;
+
+
+// let ar;
+
+// console.log( typeof ar);
+
+
+ 
+// Null;
+
+
+// let a = 10;
+// let b = null;
+// console.log(b);
+
+
+
+
+
+// Symbol;
+
+
+const screat = Symbol('screat');
+
+let user = {
+    name: 'John',
+    [screat]: "hidden mnessage"
+};
+
+
+console.log(user.name);
+console.log(screat);
+
+
+
+
+// Object;
+ 
+
+
+
+
+
+// var,  let, const 
+
+
+// var ar = 10; 
+// var ra = 45;
+
+
+// console.log(ar);
+
+
+// var ar = 30;
+
+// console.log(ar );
+
+
+// let ar = 10;
+
+// {
+//     let ar = 30;
+//     console.log(ar);
+// }
+// console.log(ar);
+
+
+
+
+
+// const ar = 10;
+
+// {
+// const ar = 30; 
+// }
+
+
+
+// let b;
+
+
+
+// let c = 30;
+
+// b = 40;
+
+
+// console.log(c+b);
+// const br;
+// br = 40;
+
+
+// console.log(br);
+
+
+
+
+
+
+
+
+
