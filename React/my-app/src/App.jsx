ea37e2f456f6f4d39b0c3eb5c6bf39e99a15d9c5
Arun raj <106869558+Hungrycoddie@users.noreply.github.com>
2025-02-01 21:55:46 +0530

    update

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Button from './Button'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello peopkle intro to react</h1>
+      <Button />
+      <Button />
+      <Button />
+
+      <Button />
+    </>
+  );
+}
+
+export default App
---
+++ React/my-app/src/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Button() {
+  return (
+      <div><button>hello im a button </button>
+     </div>
+  )
+}
+
+export default Button
